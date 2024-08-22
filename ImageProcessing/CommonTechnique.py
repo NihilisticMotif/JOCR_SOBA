f365998b8801fcf6dbb7b8e2b68b5ea01b5b60d4
Nihilisticmotif <nemocheche@gmail.com>
2024-08-22 17:19:13 +0700

    Make function name consistent.

--- ImageProcessing/CommonTechnique.py
+++ ImageProcessing/CommonTechnique.py
@@ -1,57 +1,53 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image.jpg')
-
-# get grayscale image
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+img = cv2.imread('img.jpg')
 
 # noise removal
-def remove_noise(image,n=5):
-    return cv2.medianBlur(image,n)
+def RemoveNoice(img,n=5):
+    return cv2.medianBlur(img,n)
  
 #thresholding
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+def Thresholding(img):
+    return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 #dilation
-def dilate(image):
+def Dilate(img):
     kernel = np.ones((5,5),np.uint8)
-    return cv2.dilate(image, kernel, iterations = 1)
+    return cv2.dilate(img, kernel, iterations = 1)
     
 #erosion
-def erode(image):
+def Erode(img):
     kernel = np.ones((5,5),np.uint8)
-    return cv2.erode(image, kernel, iterations = 1)
+    return cv2.erode(img, kernel, iterations = 1)
 
 #opening - erosion followed by dilation
-def opening(image,kernel=None):
+def Opening(img,kernel=None):
     if kernel==None:
         kernel = np.ones((5,5),np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
 #canny edge detection
-def canny(image):
-    return cv2.Canny(image, 100, 200)
+def Canny(img):
+    return cv2.Canny(img, 100, 200)
 
 #skew correction
-def deskew(image):
-    coords = np.column_stack(np.where(image > 0))
+def Deskew(img):
+    coords = np.column_stack(np.where(img > 0))
     angle = cv2.minAreaRect(coords)[-1]
     if angle < -45:
         angle = -(90 + angle)
     else:
         angle = -angle
-    (h, w) = image.shape[:2]
+    (h, w) = img.shape[:2]
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    rotated = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
     return rotated
 
 #template matching
-def match_template(image, template):
-    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+def MatchTemplate(img, template):
+    return cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED) 
 
 def WhiteBackGround(img):
     RangeMax=100
