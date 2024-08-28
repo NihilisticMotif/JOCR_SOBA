Fix ImageProcessing / Rotation.py bug

ImageProcessing/FFT.py
@@ -63,10 +63,10 @@ def SaveFFT(img,img_title,folder='FFT',fileformat='jpg',is_editfft=False):
     img = (img*255).astype(np.uint8) 
     SaveImage(img,img_title,folder=folder,fileformat=fileformat)
 
-def GetFFT(img,is_show=False,fft_size=None):
+def GetFFT(img,is_show=False,fft_size=None,scale=255):
     # https://docs.opencv.org/4.x/de/dbc/tutorial_py_fourier_transform.html
     img = GrayImage(img)
-    img = img / np.float32(255)
+    img = img / np.float32(scale)
     # dft is the array that contains complex numbers.
     dft = np.fft.fft2(img,fft_size)          
     # zero frequency component of dft will be at top left corner. 

ImageProcessing/Rotation.py
@@ -10,12 +10,14 @@ import Contour as tour
 
 def GetSkewAngle(
         img,
-        threshold_px=None,
-        is_binary_inv=False,
-        dilate_img=None,
-        kernel = np.ones((5,30)),
-        is_show=False,
-        is_otsu=True):
+        dilate_img      =   None,
+        threshold_px    =   None,
+        kernel          =   np.ones((2,30)),
+        kernel_area     =   9,
+        is_binary_inv   =   True,
+        is_otsu         =   True,
+        is_show         =   False
+        ):
     # https://github.com/wjbmattingly/ocr_python_textbook/blob/main/02_02_working%20with%20opencv.ipynb
     # https://becominghuman.ai/how-to-automatically-deskew-straighten-a-text-image-using-opencv-a0c30aed83df
 
@@ -35,10 +37,13 @@ def GetSkewAngle(
     if not isinstance(dilate_img,np.ndarray):
         dilate_img = tour.DefaultDilateImage(
             img=new_img,
-            threshold_px=threshold_px,
-            is_binary_inv=is_binary_inv,
-            kernel = kernel,
-            is_otsu=is_otsu)
+            threshold_px    =   threshold_px    ,   
+            kernel          =   kernel          ,   
+            kernel_area     =   kernel_area     ,   
+            is_binary_inv   =   is_binary_inv   ,   
+            is_otsu         =   is_otsu         ,   
+            is_show         =   is_show         ,   
+            )
         if is_show==True:
             show.ShowImage(dilate_img,'DefaultDilateImage')
 
@@ -83,15 +88,35 @@ def GetSkewAngle(
 
     return angle
 
-def Rotate(img, angle=None,threshold_px=None,is_binary_inv=False,dilate_img=None,is_show=False):
+def Rotate(
+        img, 
+        angle           =   None,
+        dilate_img      =   None,
+        threshold_px    =   None,
+        kernel          =   np.ones((2,30)),
+        kernel_area     =   9,
+        is_binary_inv   =   True,
+        is_otsu         =   True,
+        is_show         =   False
+        ):
     (h, w) = img.shape[:2]
     center = (w // 2, h // 2)
     if not isinstance(angle,(int,float)):
-        if not isinstance(dilate_img,np.ndarray):
-            angle = GetSkewAngle(img,threshold_px=threshold_px,is_binary_inv=is_binary_inv,is_show=is_show)
-        else:
-            angle = GetSkewAngle(img,threshold_px=threshold_px,is_binary_inv=is_binary_inv,dilate_img=dilate_img,is_show=is_show)
+        angle = GetSkewAngle(
+            img,
+            dilate_img      =   dilate_img      ,
+            threshold_px    =   threshold_px    ,
+            kernel          =   kernel          ,
+            kernel_area     =   kernel_area     ,
+            is_binary_inv   =   is_binary_inv   ,
+            is_otsu         =   is_otsu         ,
+            is_show         =   is_show         ,
+            )
     rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    if is_show==True:
+        print('rotation_matrix\n',rotation_matrix)
+        print('Reported by ImageProcessing / Rotation.py / def Rotate')
+        show.ShowImage(img,'Rotated Image')
     return cv2.warpAffine(
         img, 
         rotation_matrix, 